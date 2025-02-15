d305bc0ba595d453fef6ff02eeab08508fa10a34
Atharv Dalal <sangamdalal24@gmail.com>
2025-02-15 22:49:44 +0530

    Added Clerk Auth

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,84 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import { Button } from "./ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { BriefcaseBusiness, Heart, LogIn, PenBox } from "lucide-react";
 
 const Header = () => {
+  const [showSignIn, setShowSignIn] = useState(false);
+
+  const [search, setSearch] = useSearchParams();
+
+  useEffect(() => {
+    if (search.get("sign-in")) {
+      setShowSignIn(true);
+    }
+  }, [search]);
+
+  const handleOverClick = (e) => {
+    if (e.target === e.currentTarget) {
+      setShowSignIn(false);
+      setSearch({});
+    }
+  };
+
   return (
     <>
       <nav className="py-4 flex justify-between items-center">
         <Link>
           <img src="/logo.png" alt="" className="h-20" />
         </Link>
+
+        <div className="flex gap-8">
+          <SignedOut>
+            <Button variant="outline" onClick={() => setShowSignIn(true)}>
+              <LogIn /> Login
+            </Button>
+          </SignedOut>
+          <Link to="post-jobs">
+            <Button variant="destructive">
+              {" "}
+              <PenBox size={20} className="mr-2" /> Post a Job
+            </Button>
+          </Link>
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: { avatarBox: "w-10 h-10" },
+              }}
+            >
+              <UserButton.MenuItems>
+                <UserButton.Link
+                  label="My Jobs"
+                  labelIcon={<BriefcaseBusiness size={15} />}
+                />
+                <UserButton.Link
+                  label="Save Jobs"
+                  labelIcon={<Heart size={15} />}
+                />
+              </UserButton.MenuItems>
+            </UserButton>
+          </SignedIn>
+        </div>
       </nav>
+
+      {showSignIn && (
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={handleOverClick}
+        >
+          <SignIn
+            signUpForceRedirectUrl="/onboarding"
+            fallbackRedirectUrl="/onboarding"
+          />
+        </div>
+      )}
     </>
   );
 };
